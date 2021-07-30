4eed8073f2565a34b3ebf94bde3af24e5d71e287
r-svp26 <riteshk6341@gmail.com>
2021-07-30 14:37:21 +0530

    [Ritesh] Add: Display welcome message

---
+++ ObjectOrientedProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectOrientedProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented Programs.");
+        }
+    }
+}
